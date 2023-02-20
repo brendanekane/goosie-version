22cef7500aea9657c4188532893c554d89804885
Brendan Kane <brendanekane@gmail.com>
2023-02-20 17:18:49 -0500

    sequelize set up

---
+++ backend/config/database.js
@@ -0,0 +1,25 @@
+const config = require('./index'),
+  { db } = config,
+  { username, password, database, host } = db;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
