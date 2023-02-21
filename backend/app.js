8903204c7a23189df8fd8d70dc64c280919da042
Brendan Kane <brendanekane@gmail.com>
2023-02-21 10:20:53 -0500

    set up error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,8 @@ const express = require('express'),
   helmet = require('helmet'),
   cookieParser = require('cookie-parser'),
   { environment } = require('./config'),
-  routes = require('./routes');
+  routes = require('./routes'),
+  { ValidationError } = require('sequelize');
 
 // production boolean
 const isProduction = environment === 'production';
@@ -43,5 +44,36 @@ app.use(
 );
 // connect routes to the app
 app.use(routes);
+
+// catch unhandled http rrequests
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = 'Resource Not Found';
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors and format
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
