b9598a95f9b6678f1a0045a8fa52e437941543ef
Brendan Kane <brendanekane@gmail.com>
2023-02-21 13:58:51 -0500

    login route working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,13 @@
 const router = require('express').Router(),
-  asyncHandler = require('express-async-handler'),
-  { User } = require('../../db/models');
+  sessionRouter = require('./session'),
+  usersRouter = require('./users');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
---
+++ backend/routes/api/session.js
@@ -0,0 +1,27 @@
+const router = require('express').Router(),
+  asyncHandler = require('express-async-handler'),
+  { setTokenCookie, restoreUser } = require('../../utils/auth'),
+  { User } = require('../../db/models');
+
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    console.log(User, 'hello');
+    const { credential, password } = req.body;
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login Failed');
+      err.status = 401;
+      err.title = 'Login Failed';
+      err.errors = ['the provided credentials were invalid'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+  })
+);
+
+module.exports = router;
