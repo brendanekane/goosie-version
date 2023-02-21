17e2b6dc2ce4363105cbec59ef82e44acf221b33
Brendan Kane <brendanekane@gmail.com>
2023-02-21 14:55:43 -0500

    signup route working

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,3 +1,18 @@
-const router = require('express').Router();
+const router = require('express').Router(),
+  asyncHandler = require('express-async-handler'),
+  { setTokenCookie, requireAuth } = require('../../utils/auth'),
+  { User } = require('../../db/models');
+
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    const user = await User.signup({ username, email, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+  })
+);
 
 module.exports = router;
