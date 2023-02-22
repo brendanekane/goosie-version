32d5f3e6080f6dd70065af9881ff7825475f609d
Brendan Kane <brendanekane@gmail.com>
2023-02-22 16:28:07 -0500

    implemented dropdown on profile button to logout

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,10 +1,44 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { logoutAndRemoveSession } from '../../store/session';
 import './ProfileButton.css';
 
 const ProfileButton = () => {
+  const dispatch = useDispatch(),
+    [showMenu, setShowMenu] = useState(false),
+    sessionUser = useSelector((state) => state.session.user);
+
+  const handleIconClick = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logoutAndRemoveSession());
+    setShowMenu(false);
+  };
+
+  let dropdown;
+
+  if (showMenu) {
+    dropdown = (
+      <div className="session-dropdown-menu">
+        <p>{sessionUser.email}</p>
+        <p>{sessionUser.username}</p>
+        <button onClick={handleLogout}>logout</button>
+      </div>
+    );
+  } else {
+    dropdown = null;
+  }
+
   return (
-    <div className="profile-icon-container">
-      <i className="fa-solid fa-user"></i>
-    </div>
+    <>
+      <div className="profile-icon-container" onClick={handleIconClick}>
+        <i className="fa-solid fa-user"></i>
+      </div>
+      {dropdown}
+    </>
   );
 };
 
